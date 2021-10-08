2bf820937c0205180087e95834a3837074b0ae22
Frank Vernaillen <frank.vernaillen@ugent.be>
2021-10-08 19:38:45 +0200

    Print default values of arguments in help message

--- nis2pyr/nis2pyr.py
+++ nis2pyr/nis2pyr.py
@@ -25,15 +25,15 @@ from writer import write_pyramidal_ome_tiff
 # TODO: use logger instead of hard-coded print statements
 # TODO: provide command line flag with logging level
 
-VERSION = '0.3.3'
+VERSION = '0.3.4'
 
 
 def _parse_args():
     parser = argparse.ArgumentParser(description="Convert Nikon .nd2 image files to tiled pyramidal OME TIFF files.")
     parser.add_argument('--version', action='version', version=f'{VERSION}')
     parser.add_argument('--compression', type=str, default=None, help="the algorithm used for compressing the image data; if this flag is not specified the data will not be compressed; currently 'zlib' is the only useful algorithm, 'lzw' is not supported")
-    parser.add_argument('--pyramid-levels', type=int, default=6, help='the maximum number of resolution levels in the pyramidal OME TIFF, including the full resolution image; successive pyramid levels are downsampled by a factor 2')
-    parser.add_argument('--tile-size', type=int, default=256, help='width in pixels of the tiles in the pyramidal OME TIFF; the tiles are square; tile size must be a multiple of 16')
+    parser.add_argument('--pyramid-levels', type=int, default=6, help='the maximum number of resolution levels in the pyramidal OME TIFF, including the full resolution image; successive pyramid levels are downsampled by a factor 2 (default: %(default)d)')
+    parser.add_argument('--tile-size', type=int, default=256, help='width in pixels of the tiles in the pyramidal OME TIFF; the tiles are square; tile size must be a multiple of 16 (default: %(default)d)')
     parser.add_argument('nd2_filename', type=str, help="full filename of the input ND2 file")
     parser.add_argument('pyramid_filename', type=str, help="full filename of resulting pyramidal OME TIFF file; typically ends in .ome.tif")
     return parser.parse_args()
