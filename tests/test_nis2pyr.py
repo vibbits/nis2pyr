044d72ebfc4a1fe671c4d486c991b33ed5d8f8ef
Frank Vernaillen <frank.vernaillen@ugent.be>
2021-11-02 18:09:33 +0100

    Parametrize tester for main()

--- tests/test_nis2pyr.py
+++ tests/test_nis2pyr.py
@@ -1,25 +1,33 @@
 import os
+import pytest
 from nis2pyr.nis2pyr import main
 
 
-def test_main(mocker, easy_nd2, input_dir):
+@pytest.mark.parametrize("compression,pyramid_levels,tile_size",
+                         [(None, None, None),
+                          (None, 4, None),
+                          ("zlib", None, 512),
+                          ("zlib", 2, 128)])
+def test_main(mocker,
+              easy_nd2,
+              input_dir,
+              compression,
+              pyramid_levels,
+              tile_size):
+
     basename = os.path.basename(easy_nd2)
     input_file = os.path.join(input_dir, basename)
 
-    mocker.patch(
-        "sys.argv",
-        [
-            "nis2pyr",
-            "--compression",
-            "zlib",
-            "--pyramid-levels",
-            "4",
-            "--tile-size",
-            "512",
-            input_file
-        ]
-    )
+    args = ["nis2pyr"]
+    if compression:
+        args.extend(["--compression", compression])
+    if pyramid_levels:
+        args.extend(["--pyramid-levels", str(pyramid_levels)])
+    if tile_size:
+        args.extend(["--tile-size", str(tile_size)])
+    args.append(input_file)
 
+    mocker.patch("sys.argv", args)
     result = main()
     assert result == 0
 
